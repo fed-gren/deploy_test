1720432a4356fac671f84bb911345a02c3776fb8
fed-gren <deveb22@gmail.com>
2019-03-26 22:38:00 +0900

    Updates

---
+++ precache-manifest.4622539363c9f61e06e5be9198187475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60bf6175413607df572b",
+    "url": "/deploy_test/static/js/runtime~main.4b9d3b76.js"
+  },
+  {
+    "revision": "ded788b6bc4500488843",
+    "url": "/deploy_test/static/js/main.15a4a12d.chunk.js"
+  },
+  {
+    "revision": "0e6ca656114bb3c77457",
+    "url": "/deploy_test/static/js/2.1b58a2ad.chunk.js"
+  },
+  {
+    "revision": "ded788b6bc4500488843",
+    "url": "/deploy_test/static/css/main.e01484d4.chunk.css"
+  },
+  {
+    "revision": "46514590ec1c984bb8637a87ac9e34e3",
+    "url": "/deploy_test/index.html"
+  }
+];
